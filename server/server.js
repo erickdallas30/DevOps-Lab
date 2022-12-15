f5f6b1af93d81ecba28e10f629bc06e60c765cab
Erick Estrada <erickdallas30@gmail.com>
2022-12-15 15:26:37 -0600

    update commit

--- server/server.js
+++ server/server.js
@@ -6,6 +6,11 @@ const Rollbar = require('rollbar');
 
 const {SERVER_PORT, ROLLBAR_TOKEN} = process.env;
 
+const app = express();
+app.use(express.json());
+app.use(cors());
+app.use(express.static('public'));
+
 // include and initialize the rollbar library with your access token
 
 var rollbar = new Rollbar({
@@ -17,13 +22,25 @@ var rollbar = new Rollbar({
 // record a generic message and send it to Rollbar
 rollbar.log('Hello world!')
 
-const app = express();
-app.use(express.json());
-app.use(cors());
-app.use(express.static('public'));
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+})
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/index.html'));
-});
+app.get('/lab', function(req, res) {
+    try {
+        fakeFunction()
+        res.sendStatus(200)
+    }catch(error) {
+        rollbar.error('Not existing function call')
+        res.sendStatus(400)
+    }
+
+})
+
+
+
+// app.get('/', (req, res) => {
+//     res.sendFile(path.join(__dirname, '../public/index.html'));
+// });
 
 app.listen(SERVER_PORT, () => console.log(`Server running on Port ${SERVER_PORT}`));
